8342a9acba750cc7a8fd7c78c13cb45e114322ba
RedstoneParadox <spinjitzu00@gmail.com>
2019-12-15 18:21:36 -0800

    Replaced LibGUI with Oak Tree

--- src/main/java/redstoneparadox/cpu/CubicProcessingUnits.kt
+++ src/main/java/redstoneparadox/cpu/CubicProcessingUnits.kt
@@ -2,18 +2,16 @@ package redstoneparadox.cpu
 
 import net.fabricmc.fabric.api.container.ContainerFactory
 import net.fabricmc.fabric.api.container.ContainerProviderRegistry
-import net.minecraft.container.BlockContext
 import net.minecraft.util.Identifier
 import redstoneparadox.cpu.block.CpuBlocks
 import redstoneparadox.cpu.block.entity.CpuBlockEntityTypes
-import redstoneparadox.cpu.misc.CpuScreenController
 
 @SuppressWarnings("unused")
 fun init() {
     CpuBlockEntityTypes.register()
     CpuBlocks.register()
 
-    ContainerProviderRegistry.INSTANCE.registerFactory("cpu:cpu".id(), ContainerFactory { syncID, id, player, buf ->  CpuScreenController(syncID, player.inventory, BlockContext.create(player.world, buf.readBlockPos()))})
+    ContainerProviderRegistry.INSTANCE.registerFactory("cpu:cpu".id(), ContainerFactory { syncID, id, player, buf ->  null})
 }
 
 fun String.id(): Identifier {
--- src/main/java/redstoneparadox/cpu/client/CpuScreen.kt
+++
@@ -1,8 +0,0 @@
-package redstoneparadox.cpu.client
-
-import io.github.cottonmc.cotton.gui.client.CottonInventoryScreen
-import net.minecraft.entity.player.PlayerEntity
-import redstoneparadox.cpu.misc.CpuScreenController
-
-class CpuScreen(container: CpuScreenController, player: PlayerEntity) : CottonInventoryScreen<CpuScreenController>(container, player) {
-}
